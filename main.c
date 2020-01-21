f99d2bed3f5c574ba18cdc849e6fa30200fb9c4e
kamiabay <kamiab.ayani@gmail.com>
2020-01-20 23:50:56 -0800

    2 pipes work

--- main.c
+++ main.c
@@ -4,91 +4,133 @@
 #include <unistd.h>
 
 #define CMDLINE_MAX 512
-
-int main(void)
+void run(char *cmd)
 {
-    char cmd[CMDLINE_MAX];
-    char path[1000]; // could be any long
-    pid_t pid;
-    pid = fork();
-    if (pid == 0)
+    int i = 0;
+    char *token = strtok(cmd, " ");
+    char *arg[10];
+    while (token != NULL)
     {
-        while (1)
+        arg[i] = token;
+        i++;
+        token = strtok(NULL, " ");
+        if (token == NULL)
         {
-            char *nl;
-            int retval;
-
-            /* Print prompt */
-            printf("sshell$ ");
-            fflush(stdout);
-
-            /* Get command line */
-            fgets(cmd, CMDLINE_MAX, stdin);
-            getcwd(path, sizeof(path));
-            printf("Current working dir: %s\n", path);
-            int i = 0;
-            char *token = strtok(cmd, "<|");
-            char *commands[16];
-            while (token != NULL)
-            {
-                commands[i] = token;
-                i++;
-                token = strtok(NULL, "<|");
-            }
-
-            if (strstr(commands[0], "cd") != NULL)
-            {
-
-                char *token2 = strtok(commands[0], " ");
-                char *values[32];
-                int j = 0;
-                while (token2 != NULL)
-                {
-                    values[j] = token2;
-                    j++;
-                    token2 = strtok(NULL, " ");
-                }
-                getcwd(path, sizeof(path));
-                strcat(path, "/");
-                strcat(path, values[1]);
-                chdir(path);
-                printf("Current working dir: %s\n", path);
-                getcwd(path, sizeof(path));
-                printf("Current working dir: %s\n", path);
-            }
-
-            else
-            {
-                execvp(commands[0], commands);
-            }
-
-            /* Print command line if stdin is not provided by terminal */
-            if (!isatty(STDIN_FILENO))
-            {
-                printf("%s", cmd);
-                fflush(stdout);
-            }
-
-            /* Remove trailing newline from command line */
-            nl = strchr(cmd, '\n');
-            if (nl)
-                *nl = '\0';
-
-            /* Builtin command */
-            if (!strcmp(cmd, "exit"))
-            {
-                fprintf(stderr, "Bye...\n");
-                break;
-            }
-
-            /* Regular command */
-            retval = system(cmd);
-            fprintf(stdout, "Return status value for '%s': %d\n",
-                    cmd, retval);
+            arg[i] = token;
         }
     }
+    execvp(arg[0], arg);
+}
+void pipeline(char *process1, char *process2)
+{
+    int fd[2];
+    pipe(fd);
+    pid_t pid = fork();
+    if (pid == 0)
+    { // child
+        close(fd[1]);
+        close(STDIN_FILENO);
+        dup(fd[0]);
+        close(fd[0]);
+        run(process2);
+    }
+    else
+    { // parent
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[1]);
+        run(process1);
+    }
+}
 
-    // hiiii
+void execute(char *commands[16])
+{
+    char path[1000]; // could be any long
+    pid_t pid;
+    if (strstr(commands[0], "cd") != NULL) /// working just need to add
+    {
+        char *token2 = strtok(commands[0], " ");
+        char *values[32];
+        int j = 0;
+        while (token2 != NULL)
+        {
+            values[j] = token2;
+            j++;
+            token2 = strtok(NULL, "");
+        }
+        getcwd(path, sizeof(path));
+        strcat(path, "/");
+        strcat(path, values[1]);
+        path[strlen(path) - 2] = 0;
+        chdir(path); /// needs a space at the end to work e.g "folder "
+    }
+    else if (strstr(commands[0], "pwd") != NULL)
+    {
+        getcwd(path, sizeof(path));
+        printf("Current working dir: %s\n", path);
+    }
+    else
+    {
+        pid = fork();
+        if (pid == 0)
+        {
+            // pipeline(commands[0], commands[1]);
+            run(commands[0]);
+        }
+    }
+}
+void parse(char *cmd)
+{
+    int i = 0;
+    char *token = strtok(cmd, ">|");
+    char *commands[16];
+    while (token != NULL)
+    {
+        commands[i] = token;
+        commands[i][strlen(commands[i]) - 1] = 0;
+        i++;
+        token = strtok(NULL, ">|");
+    }
+    execute(commands);
+}
 
+void readExecute()
+{
+    char cmd[CMDLINE_MAX];
+    while (1)
+    {
+        char *nl;
+        int retval;
+        /* Print prompt */
+        printf("sshell$ ");
+        fflush(stdout);
+        /* Get command line */
+        fgets(cmd, CMDLINE_MAX, stdin);
+        parse(cmd);
+        /* Print command line if stdin is not provided by terminal */
+        if (!isatty(STDIN_FILENO))
+        {
+            printf("%s", cmd);
+            fflush(stdout);
+        }
+        /* Remove trailing newline from command line */
+        nl = strchr(cmd, '\n');
+        if (nl)
+            *nl = '\0';
+        /* Builtin command */
+        if (!strcmp(cmd, "exit"))
+        {
+            fprintf(stderr, "Bye...\n");
+            break;
+        }
+        /* Regular command */
+        //retval = system(cmd); /// nopeeeee
+        fprintf(stdout, "Return status value for '%s': %d\n",
+                cmd, retval);
+    }
+}
+int main(void)
+{
+    readExecute();
     return EXIT_SUCCESS;
 }
