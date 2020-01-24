47197a8b9f990ccadb2dae21aa652c86cda37f12
mirthyy <35956851+mirthyy@users.noreply.github.com>
2020-01-23 23:41:01 -0800

    Update sshell.c
    deleted more unnecessary comments and fixed hardcoded values for array sizes.

--- sshell.c
+++ sshell.c
@@ -9,26 +9,19 @@
 #include <sys/wait.h>
 
 #define CMDLINE_MAX 512
+#define TOKEN_MAX
 #define ARG_MAX 16
-#define MAX_SIZE_STRING 16
-///////
-//////
-/////
-////
-/// our porgram works perfectly on mac/csif YET
-// we when we use the tester or gradescoop it looks like it is not reading our output
-// please tell us what is wrong in the code, if you run this it works
 
-void printError(char *errorMessage) // prints the erros
+void printError(char *errorMessage)
 {
     fprintf(stderr, "%s", errorMessage);
     exit(-1);
 }
-char *removeWhiteSpace(char *string) // removes white spaces in a string
+char *removeWhiteSpace(char *string)
 {
     int i = 0;
     char *token = strtok(string, " ");
-    char *arg[10];
+    char *arg[ARG_MAX];
     while (token != NULL)
     {
         arg[i] = token;
@@ -41,11 +34,11 @@ char *removeWhiteSpace(char *string) // removes white spaces in a string
     }
     return arg[0];
 }
-void run(char *cmd) // a function that should work like system just gets the string and does everything and calls execvp
+void run(char *cmd)
 {
     int i = 0;
     char *token = strtok(cmd, " ");
-    char *arg[10];
+    char *arg[ARG_MAX];
     while (token != NULL)
     {
         arg[i] = token;
@@ -71,8 +64,8 @@ void pipeline(char *process1, char *process2)
     pipe(fd); // makes a little buffer area i.e the pipe
     pid_t pid = fork();
     if (pid == 0)
-    {                               // child
-        dup2(fd[1], STDOUT_FILENO); /// makes it so now it will write to the pipe
+    {// child
+        dup2(fd[1], STDOUT_FILENO); // STDOUT will now write to the pipe
         close(fd[1]);
         run(process1); // run the process // out put goes into file
     }
@@ -86,8 +79,8 @@ void pipeline(char *process1, char *process2)
 }
 void writeToFile(char *fileName, int typeOfFile)
 {
-    fileName = removeWhiteSpace(fileName); /// removes white space
-    int filedesc = open(fileName, O_RDWR | O_CREAT);
+    fileName = removeWhiteSpace(fileName);
+    int filedesc = open(fileName, O_RDWR | O_CREAT, 00700);
     if (filedesc < 0)
         printError("Error: cannot open output file\n");
     if (typeOfFile == STDERR_FILENO) // if its a type >& then send both error and output to the file
@@ -96,13 +89,20 @@ void writeToFile(char *fileName, int typeOfFile)
         dup2(filedesc, STDERR_FILENO);
     }
     else
-        dup2(filedesc, STDOUT_FILENO); // only iutput goes to the file
+        dup2(filedesc, STDOUT_FILENO); // only output goes to the file
     close(filedesc);
+}
+int isEmpty(const char *str)
+{
+    while (isspace(*str) && str++);
+    return !*str;
 }
 void redirect(char *process1, char *filename, int typeOfFile)
 {
     if (strlen(process1) == 0)
         printError("Error: missing command\n");
+	else if (is_empty(filename))
+        printError("Error: no output file\n");
     else
     {
         writeToFile(filename, typeOfFile); // points the output to the file depending if > or >&
@@ -110,14 +110,14 @@ void redirect(char *process1, char *filename, int typeOfFile)
     }
 }
 
-void execute(char *originalCommand, char *commands[16], char *type)
+void execute(char *originalCommand, char *commands[ARG_MAX], char *type)
 {
 
-    char path[1000];
-    if (strstr(commands[0], "cd") != NULL) /// does the cd function
+    char path[CMDLINE_MAX];
+    if (strstr(commands[0], "cd") != NULL)
     {
         char *token2 = strtok(commands[0], " ");
-        char *values[32];
+        char *values[TOKEN_MAX];
         int j = 0;
         while (token2 != NULL)
         {
@@ -132,22 +132,21 @@ void execute(char *originalCommand, char *commands[16], char *type)
         if (existValue != 0)
             fprintf(stderr, "Error: no such directory\n");
 
-        fprintf(stderr, "+ completed '%s %s' [%i]\n", commands[0], values[1], existValue);
+        fprintf(stderr, "+ completed '%s' [%i]\n", originalCommand, existValue);
     }
-    else if (strstr(commands[0], "pwd") != NULL) // does pwd function
+    else if (strstr(commands[0], "pwd") != NULL)
     {
         getcwd(path, sizeof(path));
-        // fflush(stdout);
         fprintf(stdout, "%s\n", path);
-        fprintf(stderr, "+ completed '%s' [0]\n", commands[0]); // works
+        fprintf(stderr, "+ completed '%s' [0]\n", originalCommand);
     }
     else
     {
         int status;
         pid_t pid;
         pid = fork();
-        if (pid == 0) // run the program in a child environment and come back to the parnet after
-        {
+        if (pid == 0) 
+        { // child process
             if (!strcmp(type, "redirect"))
                 redirect(commands[0], commands[1], STDOUT_FILENO);
             else if (!strcmp(type, "redirectError"))
@@ -160,7 +159,7 @@ void execute(char *originalCommand, char *commands[16], char *type)
             exit(1);
         }
         else if (pid > 0)
-        {
+        { // parent process
             waitpid(-1, &status, 0);
             fprintf(stderr, "+ completed '%s' [%d]\n", originalCommand, WEXITSTATUS(status));
         }
@@ -173,10 +172,10 @@ void execute(char *originalCommand, char *commands[16], char *type)
 }
 void parse(char *cmd)
 {
-    char *originalCommand = strdup(cmd);                 // duplicates the originals commands and daves it
-    originalCommand[strlen(originalCommand) - 1] = '\0'; // makes it null terminated
+    char *originalCommand = strdup(cmd); // duplicates the original command line and saves it in originalCommand
+    originalCommand[strlen(originalCommand) - 1] = '\0';
     bool isRedirect = false, isPipe = false, isRedirectError = false, isPipeError = false;
-    /// checks to see what is in the command so it can later call the correct execute fucntion
+    // checks to see what is in the command so it can later call the correct execute fucntion
     if (strstr(cmd, ">&") != NULL)
         isRedirectError = true;
     else if (strstr(cmd, "|&") != NULL)
@@ -188,16 +187,16 @@ void parse(char *cmd)
 
     int i = 0;
     char *token = strtok(cmd, ">|&");
-    char *commands[MAX_SIZE_STRING];
+    char *commands[CMDLINE_MAX];
     while (token != NULL)
     {
-        //token[strlen(token) - 1] = '\0';
         commands[i] = token;
-        commands[i][strlen(commands[i]) - 1] = '\0'; /// adds NULL to each
+        commands[i][strlen(commands[i]) - 1] = '\0'; // adds NULL to each
         i++;
         token = strtok(NULL, ">|&");
     }
 
+	// calls the correct execute function
     if (isRedirect)
         execute(originalCommand, commands, "redirect");
     else if (isRedirectError)
@@ -216,10 +215,11 @@ void readExecute()
     while (1)
     {
         char *nl;
-        // int retval;
+
         /* Print prompt */
         printf("sshell$ ");
         fflush(stdout);
+
         /* Get command line */
         fgets(cmd, CMDLINE_MAX, stdin);
         if (strstr(cmd, "exit") != NULL)
@@ -230,12 +230,14 @@ void readExecute()
         }
         else
             parse(cmd);
+
         /* Print command line if stdin is not provided by terminal */
         if (!isatty(STDIN_FILENO))
         {
             printf("%s", cmd);
             fflush(stdout);
         }
+
         /* Remove trailing newline from command line */
         nl = strchr(cmd, '\n');
         if (nl)
